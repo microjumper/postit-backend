11a27b9dd9a150b648d62a148768a262f7079897
Salvatore Sammarco <microjmpr@gmail.com>
2021-04-20 16:58:40 +0200

    get post by title

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 const port = 3000;
 
-mongoose.connect("mongodb://localhost:27017/postitdb", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/postitdb", { useNewUrlParser: true, useUnifiedTopology: true });
 const postSchema = {
     title: String,
     content: String
@@ -14,21 +14,33 @@ const Post = mongoose.model("Post", postSchema);
 
 app.get('/', (req, res) => {
     res.send('Hi there!')
-  });
-
-app.get('/posts', (req, res) => {
-    Post.find((err, posts) => err ? res.sendStatus(404) : res.json(posts))
 });
 
-app.post('/posts', (req, res) => {
-    console.log(req.body)
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-    });
-    post.save((err, post) => err ? res.sendStatus(500) : res.json(post));
-});
+app.route('/posts')
+    .get((req, res) => Post.find((err, posts) => err ? res.sendStatus(404) : res.json(posts)))
+    
+    .post((req, res) => {
+        const post = new Post({
+            title: req.body.title,
+            content: req.body.content
+        });
+        post.save((err, post) => err ? res.sendStatus(500) : res.json(post));
+    }
+);
+
+app.route('/posts/:title')
+    .get((req, res) => Post.findOne({title: req.params.title},
+        (err, post) => post ? res.json(post) : res.sendStatus(404)))
+    
+    .post((req, res) => {
+        const post = new Post({
+            title: req.body.title,
+            content: req.body.content
+        });
+        post.save((err, post) => err ? res.sendStatus(500) : res.json(post));
+    }
+);
 
 app.listen(port, () => {
-  console.log(`PostIT backend is listening at http://localhost:${port}`)
+    console.log(`PostIT backend is listening at http://localhost:${port}`)
 });
