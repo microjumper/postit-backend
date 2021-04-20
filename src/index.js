9a017c7c782e2b2d8ab77694a5a8733ab4688072
Salvatore Sammarco <microjmpr@gmail.com>
2021-04-20 15:58:05 +0200

    post post

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 mongoose.connect("mongodb://localhost:27017/postitdb", {useNewUrlParser: true, useUnifiedTopology: true});
@@ -17,6 +18,15 @@ app.get('/', (req, res) => {
 
 app.get('/posts', (req, res) => {
     Post.find((err, posts) => err ? res.sendStatus(404) : res.json(posts))
+});
+
+app.post('/posts', (req, res) => {
+    console.log(req.body)
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+    post.save((err, post) => err ? res.sendStatus(500) : res.json(post));
 });
 
 app.listen(port, () => {
